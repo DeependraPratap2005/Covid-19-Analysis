e52f8e255ce8aea82aec9c9eb11b69050048a382
Deependra Pratap Singh <sachinswati51@gmail.com>
2025-09-07 16:46:47 +0530

    covid_analysis_app.py

--- COVID-Analysis-App/covid_analysis_app.py
+++ COVID-Analysis-App/covid_analysis_app.py
@@ -16,7 +16,9 @@ st.title("COVID-19 Data Analysis and Prediction")
 st.markdown("Analyze COVID-19 cases globally with visualizations and predictive modeling.")
 
 # Add a banner or introductory image
-st.image("COVID-DATA2.JPEG", use_column_width=True, caption="Global COVID-19 Insights")
+st.image("assets/COVID-DATA2.JPEG", use_column_width=True, caption="Global COVID-19 Insights")
+
+
 
 # Sidebar for navigation
 st.sidebar.title("Navigation")
@@ -187,4 +189,3 @@ elif selected_section == "Prediction Model":
 # Footer
 st.sidebar.write("---")
 st.sidebar.info("Developed by Deependra Pratap Singh")
-
